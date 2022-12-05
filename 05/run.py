b07a2e929de4a57753bee138b807049bf4706c78
gasevic <gasevic@thch.uni-bonn.de>
2022-12-05 23:14:25 +0100

    Add Code for Day4 Part 2

--- 05/run.py
+++ 05/run.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 with open(sys.argv[-1]) as f:
     lines = f.readlines()
@@ -7,7 +8,6 @@ stacks = []
 i=0
 
 # Read in Stacks
-
 for id, line in enumerate(lines):
     if line in ['\n', '\r\n']: # Check for empty line
         empty_line=id
@@ -20,17 +20,27 @@ for id, line in enumerate(lines):
                     stacks[i].append(revline[4*x-3])
                 i=i+1
             i=0
+stacks2=copy.deepcopy(stacks)
 
-# Sort Boxes:
 for line in lines[empty_line+1:]:
     how_many=int(line.split()[1])
     fromx=int(line.split()[3])
     tox=int(line.split()[5])
+
+    # Part I:
     for x in reversed(stacks[fromx-1][-how_many:]):
         stacks[tox-1].append(x)
         del stacks[fromx-1][-1]
 
+    # Part II:
+    stacks2[tox-1]=stacks2[tox-1]+stacks2[fromx-1][-how_many:]
+    del stacks2[fromx-1][-how_many:]
+
 # Output:
 print("Part1:", end="")
 for x in stacks:
+    print(x[-1], end="")
+print("")
+print("Part2:", end="")
+for x in stacks2:
     print(x[-1], end="")
