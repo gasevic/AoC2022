88c04a46a21f25ac2e58c29bb13d7924da9b805a
gasevic <gasevic@thch.uni-bonn.de>
2022-12-11 23:29:54 +0100

    Add Code for Day 09 - Part 2

--- 09/run.py
+++ 09/run.py
@@ -10,7 +10,18 @@ gox=0
 goy=0
 h=[0,0]
 t=[0,0]
+h2=[0,0]
+k1=[0,0]
+k2=[0,0]
+k3=[0,0]
+k4=[0,0]
+k5=[0,0]
+k6=[0,0]
+k7=[0,0]
+k8=[0,0]
+k9=[0,0]
 position=[]
+position2=[]
 
 def check(h, t):
 
@@ -51,7 +62,7 @@ def check(h, t):
     return t
 
 
-
+# Part I
 for line in lines:
     if line.strip().split()[0] == "R":
         for i in range(int(line.strip().split()[1])):
@@ -77,6 +88,66 @@ for line in lines:
             check(h,t)
             position.append(t.copy())
 
+# Part II
+for line in lines:
+    if line.strip().split()[0] == "R":
+        for i in range(int(line.strip().split()[1])):
+            h2=[h2[0]+1,h2[1]]
+            temp=h2
+            check(h2,k1)
+            check(k1,k2)
+            check(k2,k3)
+            check(k3,k4)
+            check(k4,k5)
+            check(k5,k6)
+            check(k6,k7)
+            check(k7,k8)
+            check(k8,k9)
+            position2.append(k9.copy())
+
+    if line.strip().split()[0] == "L":
+        for i in range(int(line.strip().split()[1])):
+            h2=[h2[0]-1,h2[1]]
+            check(h2,k1)
+            check(k1,k2)
+            check(k2,k3)
+            check(k3,k4)
+            check(k4,k5)
+            check(k5,k6)
+            check(k6,k7)
+            check(k7,k8)
+            check(k8,k9)
+            position2.append(k9.copy())
+
+    if line.strip().split()[0] == "U":
+        for i in range(0,int(line.strip().split()[1])):
+            h2=[h2[0],h2[1]+1]
+            check(h2,k1)
+            check(k1,k2)
+            check(k2,k3)
+            check(k3,k4)
+            check(k4,k5)
+            check(k5,k6)
+            check(k6,k7)
+            check(k7,k8)
+            check(k8,k9)
+            position2.append(k9.copy())
+
+    if line.strip().split()[0] == "D":
+        for i in range(int(line.strip().split()[1])):
+            h2=[h2[0],h2[1]-1]
+            check(h2,k1)
+            check(k1,k2)
+            check(k2,k3)
+            check(k3,k4)
+            check(k4,k5)
+            check(k5,k6)
+            check(k6,k7)
+            check(k7,k8)
+            check(k8,k9)
+            position2.append(k9.copy())
+
 
 print("Part1:", len(Counter(str(elem) for elem in position)))
-#print("Part2:", max(score))
+print("Part2:", len(Counter(str(elem) for elem in position2)))
+#print("Park9:", max(score))
